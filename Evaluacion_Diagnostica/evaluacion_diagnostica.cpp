8fb2ca0a206f1eaf57ea4d9b3d6d5db47a83f00b
Alejandro <alexnachovc12@gmail.com>
2024-05-20 10:54:22 -0600

    Implementar Fraccion Suma, metodo simplifica y mcd

--- Evaluacion_Diagnostica/evaluacion_diagnostica.cpp
+++ Evaluacion_Diagnostica/evaluacion_diagnostica.cpp
@@ -15,6 +15,7 @@ public:
             cout << "El denominador no puede ser 0, se asigno 1";
             denominador = 1;
         }
+        simplifica();
     }
     /*  Fraccion(int numerador, int denominador) {
         this->numerador = numerador;
@@ -24,34 +25,44 @@ public:
     ~Fraccion() {
     }
 
-    
-
     void print() {
         cout << numerador;
         if (denominador != 1) {
            cout << "/" << denominador << endl;
         } 
     }
+
+    static Fraccion suma(Fraccion A, Fraccion B) {
+        int numNuevo, denNuevo;
+
+        numNuevo = (A.numerador * B.denominador) + (B.numerador * A.denominador);
+        denNuevo = A.denominador * B.denominador;
+
+        Fraccion C(numNuevo, denNuevo);
+        return C;
+    } 
   
 private: 
     void simplifica() {
         int mcdVal = 1;
 
-        mcdVal = mcd(numerador, denominador);
         if (denominador < 0) {
             numerador *= -1;
             denominador *= -1;
         }
 
+        mcdVal = mcd(numerador, denominador);
+        numerador /= mcdVal;
+        denominador /= mcdVal;
         
     }
     int mcd(int a, int b) {
         if (a == 0) {
             return b;
+        }    
         return mcd(b % a, a);
     }
 
-    }
     int numerador;
     int denominador;
 };
@@ -61,7 +72,7 @@ void prueba() {
 }
 
 int main() {
-    Fraccion a(-7, -6), b;
+    Fraccion a(10, 5), b;
     
     a.print();
     return 0;
