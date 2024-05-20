bdc7898100482aaef59fdda931da293440af1731
Alejandro <alexnachovc12@gmail.com>
2024-05-20 10:33:49 -0600

    Implementar metodos

--- Evaluacion_Diagnostica/evaluacion_diagnostica.cpp
+++ Evaluacion_Diagnostica/evaluacion_diagnostica.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+using namespace std;
 class Fraccion {
 public:
     Fraccion() {
@@ -8,20 +9,60 @@ public:
     }
     Fraccion(int nNumerador, int nDenominador) {
         numerador = nNumerador;
-        denominador = nDenominador;
+        if (nDenominador != 0) {
+            denominador = nDenominador;
+        } else {
+            cout << "El denominador no puede ser 0, se asigno 1";
+            denominador = 1;
+        }
     }
     /*  Fraccion(int numerador, int denominador) {
         this->numerador = numerador;
         this->denominador = denominador;
     }*/
+   
     ~Fraccion() {
+    }
+
+    
+
+    void print() {
+        cout << numerador;
+        if (denominador != 1) {
+           cout << "/" << denominador << endl;
+        } 
     }
   
 private: 
+    void simplifica() {
+        int mcdVal = 1;
+
+        mcdVal = mcd(numerador, denominador);
+        if (denominador < 0) {
+            numerador *= -1;
+            denominador *= -1;
+        }
+
+        
+    }
+    int mcd(int a, int b) {
+        if (a == 0) {
+            return b;
+        return mcd(b % a, a);
+    }
+
+    }
     int numerador;
     int denominador;
 };
 
+void prueba() {
+    Fraccion e(1, 2);
+}
+
 int main() {
+    Fraccion a(-7, -6), b;
+    
+    a.print();
     return 0;
 }
