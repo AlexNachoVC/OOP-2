69d58cc4dd0d1dcf8600a39bc791acfaf7ad33c5
Alejandro <alexnachovc12@gmail.com>
2024-05-23 10:04:10 -0600

    Mover del archivo main.cpp, al archivo Fraccion.h la implementacion de la clase Fraccion

---
+++ Evaluacion_Diagnostica/Fraccion.h
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+class Fraccion {
+public:
+    Fraccion() {
+        numerador = 0;
+        denominador = 1;
+    }
+    Fraccion(int nNumerador, int nDenominador) {
+        numerador = nNumerador;
+        if (nDenominador != 0) {
+            denominador = nDenominador;
+        } else {
+            cout << "El denominador no puede ser 0, se asigno 1";
+            denominador = 1;
+        }
+        simplifica();
+    }
+    /*  Fraccion(int numerador, int denominador) {
+        this->numerador = numerador;
+        this->denominador = denominador;
+    }*/
+   
+    ~Fraccion() {
+    }
+
+    void print() {
+        cout << numerador;
+        if (denominador != 1) {
+           cout << "/" << denominador << endl;
+        } 
+    }
+
+    static Fraccion suma(Fraccion A, Fraccion B) {
+        int numNuevo, denNuevo;
+
+        numNuevo = (A.numerador * B.denominador) + (B.numerador * A.denominador);
+        denNuevo = A.denominador * B.denominador;
+
+        Fraccion C(numNuevo, denNuevo);
+        return C;
+    } 
+  
+private: 
+    void simplifica() {
+        int mcdVal = 1;
+
+        if (denominador < 0) {
+            numerador *= -1;
+            denominador *= -1;
+        }
+
+        mcdVal = mcd(numerador, denominador);
+        numerador /= mcdVal;
+        denominador /= mcdVal;
+        
+    }
+    int mcd(int a, int b) {
+        if (a == 0) {
+            return b;
+        }    
+        return mcd(b % a, a);
+    }
+
+    int numerador;
+    int denominador;
+};
--- Evaluacion_Diagnostica/evaluacion_diagnostica.cpp
+++ Evaluacion_Diagnostica/evaluacion_diagnostica.cpp
@@ -1,78 +1,10 @@
 #include <iostream>
+#include "Fraccion.h"
 
 using namespace std;
-class Fraccion {
-public:
-    Fraccion() {
-        numerador = 0;
-        denominador = 1;
-    }
-    Fraccion(int nNumerador, int nDenominador) {
-        numerador = nNumerador;
-        if (nDenominador != 0) {
-            denominador = nDenominador;
-        } else {
-            cout << "El denominador no puede ser 0, se asigno 1";
-            denominador = 1;
-        }
-        simplifica();
-    }
-    /*  Fraccion(int numerador, int denominador) {
-        this->numerador = numerador;
-        this->denominador = denominador;
-    }*/
-   
-    ~Fraccion() {
-    }
-
-    void print() {
-        cout << numerador;
-        if (denominador != 1) {
-           cout << "/" << denominador << endl;
-        } 
-    }
-
-    static Fraccion suma(Fraccion A, Fraccion B) {
-        int numNuevo, denNuevo;
-
-        numNuevo = (A.numerador * B.denominador) + (B.numerador * A.denominador);
-        denNuevo = A.denominador * B.denominador;
-
-        Fraccion C(numNuevo, denNuevo);
-        return C;
-    } 
-  
-private: 
-    void simplifica() {
-        int mcdVal = 1;
-
-        if (denominador < 0) {
-            numerador *= -1;
-            denominador *= -1;
-        }
-
-        mcdVal = mcd(numerador, denominador);
-        numerador /= mcdVal;
-        denominador /= mcdVal;
-        
-    }
-    int mcd(int a, int b) {
-        if (a == 0) {
-            return b;
-        }    
-        return mcd(b % a, a);
-    }
-
-    int numerador;
-    int denominador;
-};
-
-void prueba() {
-    Fraccion e(1, 2);
-}
 
 int main() {
-    Fraccion a(17, 2), b(3,4), c;
+    Fraccion a(17, 2), b(3,4), c, arreglo[5];
     
     a.print();
     b.print();
