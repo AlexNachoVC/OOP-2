b654b596d5788980c895e7d92cb861429cae3261
Alejandro <alexnachovc12@gmail.com>
2024-06-09 14:57:18 -0600

    Implementacion clase Video

---
+++ Situacion_Problema/Video.cpp
@@ -0,0 +1,43 @@
+#include "Video.h"
+
+using namespace std;
+
+Video::Video(int id, string nombre, int duracion, string genero) {
+        this->id = id;
+        this->nombre = nombre;
+        this->duracion = duracion;
+        this->genero = genero;
+        this->calificacion = 0.0; 
+}
+
+int Video::getId() {
+    return id;
+}
+
+string Video::getNombre() {
+    return nombre;
+}
+
+int Video::getDuracion() {
+    return duracion;
+}
+
+string Video::getGenero() {
+    return genero;
+}
+
+float Video::getCalificacion() {
+    return calificacion;
+}
+
+void Video::setCalificacion(float calificacion) {
+    this->calificacion = calificacion;
+}
+
+void Video::mostrar() {
+    cout << "ID: " << id << " | "
+         << "Nombre: " << nombre << " | "
+         << "Duracion: " << duracion << " | "
+         << "Genero: " << genero << " | "
+         << "Calificacion: " << calificacion << endl;
+}
---
+++ Situacion_Problema/Video.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Video {
+protected: 
+    int id;
+    string nombre;
+    int duracion;
+    string genero;
+    float calificacion;
+
+public: 
+    Video(int id, string nombre, int duracion, string genero);
+    int getId();
+    string getNombre();
+    int getDuracion();
+    string getGenero();
+    float getCalificacion();
+    void setCalificacion(float calificacion);
+    virtual void mostrar();
+};
