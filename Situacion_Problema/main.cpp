c363e7a164b8edb9168b802e6162e3fe304e291b
Alejandro <alexnachovc12@gmail.com>
2024-06-10 20:15:35 -0600

    Implementacion de los casos faltantes del menu. Proyecto terminado!

--- Situacion_Problema/main.cpp
+++ Situacion_Problema/main.cpp
@@ -8,64 +8,75 @@
 
 using namespace std;
 
+Pelicula *peliculaArray = nullptr;
+int dataSizePelicula = 0;
+
+Episodio *episodioArray = nullptr;
+int dataSizeSerie = 0;
+
+
 int main() {
-    Pelicula *peliculaArray = nullptr;
-    int dataSizePelicula = 0;
-
-    dataSizePelicula = countDataLinesInCSV(MOVIES_FILE);
-    if(dataSizePelicula == -1) {
-        cerr << "No se pudo cargar el data set de " << MOVIES_FILE << "\n";
-        return 0;
-    }
-
-    cout << "movies.csv tiene: " << dataSizePelicula << " entradas\n";
-    peliculaArray = new(nothrow) Pelicula[dataSizePelicula];
-    if(peliculaArray == nullptr) {
-        cerr << "No hubo memoria para el arreglo de " << MOVIES_FILE << "\n";
-        return 0;
-    }
-
-    if(!loadMoviesFromCSV(MOVIES_FILE, peliculaArray, dataSizePelicula)) {
-        cerr << "Error al cargar el data set de " << MOVIES_FILE << "\n";
-        delete [] peliculaArray;
-        return 0;
-    }
-
-
-    Episodio *episodioArray = nullptr;
-    int dataSizeSerie = 0;
-
-    dataSizeSerie = countDataLinesInCSV(SERIES_FILE);
-    if(dataSizeSerie == -1) {
-        cerr << "No se pudo cargar el data set de " << SERIES_FILE << "\n";
-        return 0;
-    }
-
-    cout << "series.csv tiene: " << dataSizeSerie << " entradas\n";
-    episodioArray = new(nothrow) Episodio[dataSizeSerie];
-    if(episodioArray == nullptr) {
-        cerr << "No hubo memoria para el arreglo de " << SERIES_FILE << "\n";
-        return 0;
-    }
-
-    if(!loadSeriesFromCSV(SERIES_FILE, episodioArray, dataSizeSerie)) {
-        cerr << "Error al cargar el data set de " << SERIES_FILE << "\n";
-        delete [] episodioArray;
-        return 0;
-    }
-
+  
     int opcion = 0;
     while (opcion != 6) {
-        cout << "1. Mostrar videos con cierta calificacion o genero\n";
-        cout << "2. Mostrar episodios de una serie con cierta calificacion\n";
-        cout << "3. Mostrar peliculas con cierta calificacion\n";
-        cout << "4. Calificar un video\n";
-        cout << "5. Salir\n";
+        cout << "\n";
+        cout << "1. Cargar archivo de datos\n";
+        cout << "2. Mostrar videos con cierta calificacion o genero\n";
+        cout << "3. Mostrar episodios de una serie con cierta calificacion\n";
+        cout << "4. Mostrar peliculas con cierta calificacion\n";
+        cout << "5. Calificar un video\n";
+        cout << "6. Salir\n";
         cout << "Selecciona una opcion: ";
         cin >> opcion;
 
         switch (opcion) {
             case 1:
+                {
+                    dataSizePelicula = countDataLinesInCSV(MOVIES_FILE);
+                    if(dataSizePelicula == -1) {
+                        cerr << "No se pudo cargar el data set de " << MOVIES_FILE << "\n";
+                        return 0;
+                    }
+
+                    cout << "movies.csv tiene: " << dataSizePelicula << " entradas\n";
+                    peliculaArray = new(nothrow) Pelicula[dataSizePelicula];
+                    if(peliculaArray == nullptr) {
+                        cerr << "No hubo memoria para el arreglo de " << MOVIES_FILE << "\n";
+                        return 0;
+                    }
+
+                    if(!loadMoviesFromCSV(MOVIES_FILE, peliculaArray, dataSizePelicula)) {
+                        cerr << "Error al cargar el data set de " << MOVIES_FILE << "\n";
+                        delete [] peliculaArray;
+                        return 0;
+                    }
+
+
+                    dataSizeSerie = countDataLinesInCSV(SERIES_FILE);
+                    if(dataSizeSerie == -1) {
+                        cerr << "No se pudo cargar el data set de " << SERIES_FILE << "\n";
+                        return 0;
+                    }
+
+                    cout << "series.csv tiene: " << dataSizeSerie << " entradas\n";
+                    episodioArray = new(nothrow) Episodio[dataSizeSerie];
+                    if(episodioArray == nullptr) {
+                        cerr << "No hubo memoria para el arreglo de " << SERIES_FILE << "\n";
+                        return 0;
+                    }
+
+                    if(!loadSeriesFromCSV(SERIES_FILE, episodioArray, dataSizeSerie)) {
+                        cerr << "Error al cargar el data set de " << SERIES_FILE << "\n";
+                        delete [] episodioArray;
+                        return 0;
+                    }
+
+                    cout << "Archivos de datos cargados de manera exitosa!" << endl;
+
+                    break;
+                }
+
+            case 2:
                 {
                     cout <<  "Quiere ver los videos en base a al genero a en base a la calificacion?" << endl << "a) Genero" << endl << "b) Calificacion" << endl;
                     char seleccion;
@@ -75,7 +86,7 @@ int main() {
                     if (tolower(seleccion) == 'a') {
                         cout << "Opciones de genero: " << endl << " 1. Drama\n 2. Accion\n 3. Misterio" << endl;
                         int seleccion_genero;  
-                        cout << "Selecciona el genero: ";
+                        cout << "Selecciona el genero (Ingresa el numero correspondiente): ";
                         cin >> seleccion_genero; 
 
                         string genero;
@@ -126,10 +137,11 @@ int main() {
                     }
                     break;
                 }
-            case 2:
+
+            case 3:
                 {
                     cout << "De que serie le interesa conocer los episodios con cierta calificacion?" << endl;
-                    cin.ignore(); // Ignora el carácter de nueva línea que queda en el búfer de entrada después de usar cin. ya que puede causar problemas en la continuacion del codigo el no borrarlo 
+                    cin.ignore(); // Ignora el carácter de nueva línea que queda en el búfer de entrada después de usar cin. ya que puede causar problemas en la continuacion del codigo el no borrarlo. (Esto lo tuve que investigar por un error, y asi llegue a la solucion. Fuente: https://stackoverflow.com/questions/25475384/when-and-why-do-i-need-to-use-cin-ignore-in-c)
                     string seleccion_serie;
                     getline(cin, seleccion_serie); // Lee una linea entera de entrada del usuario, ya que "cin" solo lee una palabra, por lo que, al estar el nombre de la serie conformado por dos palabras, causaba problemas usar cin.
 
@@ -145,7 +157,8 @@ int main() {
                     }
                     break;
                 }
-            case 3:
+                
+            case 4:
                 {
                     cout << "Con que calificacion te gustaria que te mostrara peliculas? " << endl;
                     float seleccion_calificacion;
@@ -157,17 +170,73 @@ int main() {
                         }
                     }
 
+                    break;
                 }
-                break;
-            case 4:
                 
-                break;
             case 5:
-                std::cout << "Saliendo...\n";
-                break;
+                {
+                    cout << "Deseas calificar una pelicula o un episodio? (p/e)" << endl;
+                    char tipo_video;
+                    cin >> tipo_video;
+                    cin.ignore();
+
+                    string nombre_video;
+                    string titulo_episodio;
+
+                    if (tipo_video == 'p') {
+                        cout << "Ingresa el nombre de la pelicula que deseas calificar: " << endl;
+                        getline(cin, nombre_video);
+                    } 
+                    else if (tipo_video == 'e') {
+                        cout << "Ingresa el nombre de la serie de la cual deseas calificar un episodio: " << endl;
+                        getline(cin, nombre_video);
+
+                        cout << "Ingresa el titulo del episodio que deseas calificar: " << endl;
+                        getline(cin, titulo_episodio);
+                    } 
+                    else {
+                        cout << "Tipo de video no valido. Por favor, intenta de nuevo." << endl;
+                        break;
+                    }
+
+                    cout << "Ingresa la nueva calificacion: " << endl;
+                    float nueva_calificacion;
+                    cin >> nueva_calificacion;
+                    cin.ignore();
+
+                    if (tipo_video == 'p') {
+                        for (int i = 0; i < dataSizePelicula; i++) {
+                            if (peliculaArray[i].getNombre() == nombre_video) {
+                                peliculaArray[i].setCalificacion(nueva_calificacion);
+                                cout << "Calificacion actualizada con exito!" << endl;
+                                break;
+                            }  
+                        }
+                    }
+                    else if (tipo_video == 'e') {
+                        for (int i = 0; i < dataSizeSerie; i++) {
+                            if (episodioArray[i].getNombre() == nombre_video && episodioArray[i].getTitulo() == titulo_episodio) {
+                                episodioArray[i].setCalificacion(nueva_calificacion);
+                                cout << "Calificacion actualizada con exito!" << endl;
+                                break;
+                            }
+                        }
+                    }
+
+                    break;
+                }
+                
+            case 6:
+                {
+                    cout << "Saliendo...\n";
+                    break;
+                }
+                
             default:
-                std::cout << "Opcion no valida. Por favor, intenta de nuevo.\n";
-                break;
+                {
+                    cout << "Opcion no valida. Por favor, intenta de nuevo.\n";
+                    break;
+                }
         }
     }
 }
